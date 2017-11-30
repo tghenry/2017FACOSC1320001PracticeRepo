0290a4654c29891012e3cdb40dd775ec43a44278
tghenry <r3ap3rofgames@yahoo.com>
2017-11-30 13:47:10 -0600

    This is my initial project.

---
+++ GithubExample/GithubExample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This is my Hello World
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
